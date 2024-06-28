dafab1068ea3d90847670b82fba4debf33ef799c
juni <juniejere6654@gmail.com>
2024-06-28 11:49:57 -0500

    modified projects page, will most likely add bubbles as empty floating bubbles over page animation with negative z index so that it does not cover the content

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,53 +1,53 @@
 import React, { useState } from "react";
 import "./bubbles.css";
-import home from "./assets/home.png";
-import eStoreDB from "./assets/eStoreDB.png";
-import buildMyPortfolio from "./assets/buildMyPortfolio.png";
-import dinoDetectors from "./assets/dinoDetectors.png";
+import delivered from "./assets/delivered2.png";
+import eStoreDB from "./assets/eStoreDB2.png";
+import buildMyPortfolio from "./assets/buildMyPortfolio2.png";
+import dinoDetectors from "./assets/dinoDetectors2.png";
 // import Bubbles from "./Bubbles";
-import Bubbles from "./Bubbles2";
+// import Bubbles from "./Bubbles2";
 
 export default function Projects() {
   const projects = [
     {
       projectName: "Delivered",
-      image: home,
-      // description: "This is a project that was delivered",
+      image: delivered,
+      description: "This is a project that was delivered",
     },
     {
       projectName: "BuildMyPortfolio",
       image: buildMyPortfolio,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
     {
       projectName: "E-Store Database",
       image: eStoreDB,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
     {
       projectName: "Dino Detectors",
       image: dinoDetectors,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
     {
       projectName: "Delivered",
-      image: home,
-      // description: "This is a project that was delivered",
+      image: delivered,
+      description: "This is a project that was delivered",
     },
     {
       projectName: "BuildMyPortfolio",
       image: buildMyPortfolio,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
     {
       projectName: "E-Store Database",
       image: eStoreDB,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
     {
       projectName: "Dino Detectors",
       image: dinoDetectors,
-      // description: "This is a project that was delivered",
+      description: "This is a project that was delivered",
     },
   ];
 
@@ -68,12 +68,26 @@ export default function Projects() {
   const displayProjects = () => {
     return projects.map((project, index) => {
       return (
-        <Bubbles
-          key={index}
-          index={index}
-          project={project.projectName}
-          image={project.image}
-        />
+        // <Bubbles
+        //   key={index}
+        //   index={index}
+        //   project={project.projectName}
+        //   image={project.image}
+        // />
+        <div className="projectDiv" key={index}>
+          <div className="imgContainer">
+            <img src={project.image} alt="project" className="projectImage" />
+          </div>
+
+          <div className="projectDesc">
+            <div className="projectInfo">
+              <h3>{project.projectName}</h3>
+              <p>{project.description}</p>
+            </div>
+
+            <div className="bubbleBtn">View Project</div>
+          </div>
+        </div>
       );
     });
   };
