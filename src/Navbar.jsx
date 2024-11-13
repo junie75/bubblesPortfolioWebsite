3c421d22396aeba76c4721f76d19761ecdd5441d
juni <juniejere6654@gmail.com>
2024-11-13 09:43:20 -0600

    fixed navbar, home page, about page, and projects page to be more mobile responsive. Added mobile menu for smaller screens

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [anchorElement, setAnchorElement] = useState(null);
+
+  const handleClose = () => {
+    setAnchorElement(null);
+  };
+
+  const handleOpen = (event) => {
+    setAnchorElement(event.currentTarget);
+  };
+
   return (
     <div className="navContainer">
+      <div className="mobileMenu">
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleOpen}
+        >
+          <span class="material-symbols-outlined">menu</span>
+        </Button>
+        <Menu
+          keepMounted
+          anchorEl={anchorElement}
+          onClose={handleClose}
+          open={Boolean(anchorElement)}
+        >
+          <MenuItem onClick={handleClose}>
+            <Link to={"/"}>Home</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link to={"/about"}>About</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link to={"/projects"}>Projects</Link>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link to={"/contact"}>Contact</Link>
+          </MenuItem>
+        </Menu>
+      </div>
       <div className="nav">
         <a href="/">Home</a>
         <a href="/about">About</a>
